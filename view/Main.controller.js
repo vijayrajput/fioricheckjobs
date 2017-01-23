39cb0e08155117f645eca467e885d3f17cb3cbb3
Vijay Singh Rajput <vijay.rajput@gmail.com>
2017-01-23 14:37:10 +0530

    Update Main.controller.js

--- view/Main.controller.js
+++ view/Main.controller.js
@@ -1,4 +1,4 @@
-sap.ui.controller("com.sap.sdc.hcp.bootcamp.view.Main", {
+sap.ui.controller("com.sap.sdc.hcp.bootcamp1.view.Main", {
 
 	onInit: function() {
 		if (sap.ui.Device.support.touch === false) {
